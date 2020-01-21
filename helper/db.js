19bcbddebfd14a90592e79a6d23fe00d8816b7b6
Ahmet <ahmetcilingir@outlook.com>
2020-01-21 12:46:01 +0300

    dbConnection

---
+++ helper/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb+srv://movie_user:onFgPpl8RqkCLAam@cluster0-v9ufy.mongodb.net/test?retryWrites=true&w=majority');
+
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB Connection Başarılı');
+    });
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB Bağlantı Başarısız')
+    })
+}

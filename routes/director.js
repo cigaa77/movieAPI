bec6519a27d18da7fdcea28e15cadeb86f58bd91
Ahmet <ahmetcilingir@outlook.com>
2020-01-22 10:08:21 +0300

    Create a new director

--- routes/director.js
+++ routes/director.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ title: 'Director' })
+// Models
+const Director=require('../models/Director');
+
+router.post('/', (req, res) => {
+  const director = new Director(req.body);
+  director.save()
+    .then(data => {
+      res.json(data)
+    })
+    .catch(err => {
+      res.json(err)
+    })
 })
 
 module.exports = router;

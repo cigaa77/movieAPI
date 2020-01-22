0902208b8a839e49b86082940d849c491b9d5d5f
Ahmet <ahmetcilingir@outlook.com>
2020-01-22 09:46:39 +0300

    Remove a Movie

--- routes/movie.js
+++ routes/movie.js
@@ -45,4 +45,14 @@ router.put('/id/:movie_id', (req, res) => {
     })
 })
 
+router.delete('/id/:movie_id', (req, res) => {
+  Movie.findByIdAndRemove(req.params.movie_id)
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => {
+      res.json(err)
+    })
+})
+
 module.exports = router;

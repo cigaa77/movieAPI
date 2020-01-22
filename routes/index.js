884f6f73edbd71636b86a924051d037e1590fef3
Ahmet <ahmetcilingir@outlook.com>
2020-01-22 18:03:12 +0300

    Bcryptjs ile Password Şifreleme

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var bcryptjs = require('bcryptjs');
+
 // Models
 const User = require('../models/User')
 
@@ -11,14 +13,17 @@ router.get('/', function (req, res, next) {
 
 router.post('/register', function (req, res, next) {
   const { username, password } = req.body;
-  const user = new User({
-    username,
-    password
-  });
-  user.save().then(data => {
-    res.json(data)
-  }).catch(err => {
-    res.json(err)
+
+  bcryptjs.hash(password, 10).then((hash) => {
+    const user = new User({
+      username,
+      password: hash
+    });
+    user.save().then(data => {
+      res.json(data)
+    }).catch(err => {
+      res.json(err)
+    })
   })
 });
 

422b3901e46b0cfc0700950b1c3ff99aafca53a2
Ahmet <ahmetcilingir@outlook.com>
2020-01-22 19:18:08 +0300

    jsonwebtoken ile Token Oluşturma

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Models
 const User = require('../models/User')
@@ -24,6 +25,42 @@ router.post('/register', function (req, res, next) {
     }).catch(err => {
       res.json(err)
     })
+  })
+});
+
+router.post('/authenticate', (req, res) => {
+  const { username, password } = req.body;
+  User.findOne({
+    username
+  }, (err, user) => {
+    if (err)
+      throw err;
+    if (!user) {
+      res.json({
+        status: false,
+        message: 'Authentication failed, user not found.'
+      })
+    } else {
+      bcryptjs.compare(password, user.password).then(result => {
+        if (!result) {
+          res.json({
+            status: false,
+            message: 'Authentication failed, wrong password.'
+          })
+        } else {
+          const payload = {
+            username
+          };
+          const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+            expiresIn: 720 // 12 saat ( 720 dk )
+          });
+          res.json({
+            status: true,
+            token
+          });
+        }
+      })
+    }
   })
 });
 

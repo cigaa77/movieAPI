bda52d517699a8d40a1bd1e2623d62194706cf50
Ahmet <ahmetcilingir@outlook.com>
2020-01-22 17:52:41 +0300

    Kullanıcı Kayıt İşlemi

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+// Models
+const User = require('../models/User')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', function (req, res, next) {
+  const { username, password } = req.body;
+  const user = new User({
+    username,
+    password
+  });
+  user.save().then(data => {
+    res.json(data)
+  }).catch(err => {
+    res.json(err)
+  })
 });
 
 module.exports = router;

af0dd8dfd5fb9b8e8eca04b1e87e4d526c06c292
Ahmet <ahmetcilingir@outlook.com>
2020-01-23 09:06:35 +0300

    mongoose Deprecition Warning Edit

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://movie_user:onFgPpl8RqkCLAam@cluster0-v9ufy.mongodb.net/test?retryWrites=true&w=majority');
+    mongoose.connect('mongodb+srv://movie_user:onFgPpl8RqkCLAam@cluster0-v9ufy.mongodb.net/test?retryWrites=true&w=majority',
+        { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
     mongoose.connection.on('open', () => {
         console.log('MongoDB Connection Başarılı');

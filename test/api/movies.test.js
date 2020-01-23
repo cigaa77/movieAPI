ef41f15e6bf3077e4bf946649d85dc19a8dc0f7a
Ahmet <ahmetcilingir@outlook.com>
2020-01-23 14:41:08 +0300

    movie_id Get-TEST

--- test/api/movies.test.js
+++ test/api/movies.test.js
@@ -5,7 +5,7 @@ const server = require('../../app');
 
 chai.use(chaiHttp);
 
-let token;
+let token, movie_id;
 
 describe('/api/movies Tests', () => {
     before((done) => {
@@ -53,6 +53,26 @@ describe('/api/movies Tests', () => {
                     res.body.should.have.property('country');
                     res.body.should.have.property('year');
                     res.body.should.have.property('imdb_score');
+                    movie_id = res.body._id;
+                    done();
+                })
+        })
+    })
+    describe('/GET/:movie_id movie', () => {
+        it('it should  GET a movie by the given id', (done) => {
+            chai.request(server)
+                .get('/api/movies/id/' + movie_id)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('_id').eql(movie_id);
                     done();
                 })
         })
